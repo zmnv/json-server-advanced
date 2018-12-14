5702554dbf195daa5cc5a628e0c7746ced3723ec
Valerij Zimnev <sorption.music@gmail.com>
2018-12-15 02:15:34 +0700

    Fix

--- custom-routes.js
+++ custom-routes.js
@@ -1,17 +1,16 @@
 const jsonServer = require('json-server');
 const router = jsonServer.create();
 
-const RestartServer = require('./config/restarter');
-const DeployAndRestartServer = require('./config/restarter');
+const Restarter = require('./config/restarter');
 
 router.post('/restart', function (req, res) {
   res.json('Server Restart');
-  RestartServer();
+  Restarter.RestartServer();
 });
 
 router.post('/deploy', function (req, res) {
   res.json('Server Deploy');
-  DeployAndRestartServer();
+  Restarter.DeployAndRestartServer();
 });
 
 module.exports = {
