363e3a5407122844d7c82b36044231ee3b390c4b
Valerij Zimnev <sorption.music@gmail.com>
2018-12-15 02:31:21 +0700

    test

--- config/restarter.js
+++ config/restarter.js
@@ -14,8 +14,10 @@ function RestartServer() {
 
 function DeployAndRestartServer() {
     console.log(`${new Date()} \x1b[93mINCOMING COMMAND: DEPLOY SERVER\x1b[0m`);
+    exec("pm2 stop json-server-3000", () => console.log('\x1b[91mStop Server\x1b[0m'));
     exec("cd /home/json-server-advanced", puts);
-    exec("ls -l", puts);
+    exec("git pull", puts);
+    exec("pm2 start json-server-3000", () => console.log('\x1b[92mStart Server\x1b[0m'));
     // RestartServer();
 }
 
